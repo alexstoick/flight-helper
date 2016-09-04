82e007c28371f0b87c43d641e884b06f2e67d6c8
Stoica Alex <alex_stoicajr@yahoo.co.uk>
2016-09-04 11:44:30 +0100

    time parsed

--- itinerary.rb
+++ itinerary.rb
@@ -1,10 +1,14 @@
 class Itinerary
-  attr_reader :outbound_leg_id, :price, :deeplink_url
+  attr_reader :outbound_leg_id, :inbound_leg_id, :price, :deeplink_url
   attr_accessor :outbound_leg
   attr_accessor :outbound_carrier
-  attr_accessor :origin, :destination
+  attr_accessor :inbound_origin, :inbound_destination
+  attr_accessor :outbound_origin, :outbound_destination
+  attr_accessor :inbound_leg, :inbound_carrier
+
   def initialize(json)
     @outbound_leg_id = json["OutboundLegId"]
+    @inbound_leg_id = json["InboundLegId"]
     pricing_option = json["PricingOptions"][0]
     @price = pricing_option["Price"]
     @deeplink_url = pricing_option["DeeplinkUrl"]
@@ -15,14 +19,27 @@ class Itinerary
   end
 
   def to_json
+    hash = default_hash
+    if inbound_leg
+      hash.merge(
+        {
+          inbound_carrier: inbound_carrier.name,
+          inbound: "#{inbound_origin.name} - #{inbound_destination.name}",
+          inbound_time: "#{inbound_leg.time_difference}",
+        }
+      )
+    end
+    hash
+  end
+
+  private
+  def default_hash
     {
       price: price,
       stops: stops,
       outbound_carrier: outbound_carrier.name,
-      departure: outbound_leg.departure,
-      arrival: outbound_leg.arrival,
-      origin: origin.name,
-      destination: destination.name,
+      outbound: "#{outbound_origin.name} - #{outbound_destination.name}",
+      outbound_time: "#{outbound_leg.time_difference}",
     }
   end
 end
--- leg.rb
+++ leg.rb
@@ -1,15 +1,30 @@
+require 'date'
 class Leg
   attr_reader :id,:departure, :arrival, :carriers, :stops, :duration,
-    :origin_id, :destination_id
+    :origin_id, :destination_id, :time_difference
 
   def initialize(json)
     @id = json["Id"]
-    @departure = json["Departure"]
-    @arrival = json["Arrival"]
+    @departure = DateTime.parse(json["Departure"])
+    @arrival = DateTime.parse(json["Arrival"])
     @stops = json["Stops"].count
     @carriers = json["OperatingCarriers"]
     @duration = json["Duration"]
     @origin_id = json["OriginStation"]
     @destination_id = json["DestinationStation"]
+    @time_difference = distance_of_time_in_hours_and_minutes(@duration)
+  end
+
+  private
+
+  def distance_of_time_in_hours_and_minutes(dist)
+    minutes = dist.abs
+    hours = minutes / 60
+    minutes = minutes - (hours * 60)
+
+    words = ""
+
+    words << "#{hours} #{hours > 1 ? 'hours' : 'hour' } and " if hours > 0
+    words << "#{minutes} #{minutes == 1 ? 'minute' : 'minutes' }"
   end
 end
